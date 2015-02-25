39d73dcb46badc52424f2c3bc859b3b14b26c5a5
blissland <blissland.house@googlemail.com>
2015-02-25 09:59:18 +0000

    Added bonjour and small fixes

--- configure.sh
+++ configure.sh
@@ -6,21 +6,21 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+# Update to latest packages
+apt-get update
+
 # Install node.js
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
 dpkg -i node_latest_armhf.deb
 rm node_latest_armhf.deb
 
 # Install latest omxplayer
-wget http://omxplayer.sconde.net/builds/omxplayer_0.3.6~git20150128~18f051d_armhf.deb
-dpkg -i omxplayer_0.3.6~git20150128~18f051d_armhf.deb
-rm omxplayer_0.3.6~git20150128~18f051d_armhf.deb
+wget -O omxplayer.deb http://omxplayer.sconde.net/builds/omxplayer_0.3.6~git20150217~5337be8_armhf.deb
+dpkg -i omxplayer.deb
+rm omxplayer.deb
 
 # Install peerflix
 npm install -g peerflix
-
-# Update to latest packages
-apt-get update
 
 # Install GIT
 apt-get -y install git
@@ -63,3 +63,6 @@ apt-get -y install libcap2-bin
 
 # So server can run on port 80 without sudo
 setcap 'cap_net_bind_service=+ep' /usr/bin/python2.7
+
+# Install bonjour for raspberrypi.local 
+apt-get -y install libnss-mdns
