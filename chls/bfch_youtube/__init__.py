000d3d0d989d127a0b832725a6e3fe63c3501599
blissland <blissland.house@googlemail.com>
2015-05-07 10:36:48 +0000

    Update to youtube api v3

--- chls/bfch_youtube/__init__.py
+++ chls/bfch_youtube/__init__.py
@@ -2,19 +2,11 @@ import chanutils.reddit
 from chanutils import get_json
 from playitem import PlayItem, PlayItemList
 
-_SEARCH_URL = "http://gdata.youtube.com/feeds/api/videos"
+_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
 _FEEDLIST = [
   {'title':'Trending', 'url':'http://www.reddit.com/domain/youtube.com/top/.json'},
-  {'title':'Popular Today', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular?time=today&v=2&alt=jsonc'},
-  {'title':'Animals', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Animals?time=today&v=2&alt=jsonc'},
-  {'title':'Comedy', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Comedy?time=today&v=2&alt=jsonc'},
-  {'title':'Entertainment', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Entertainment?time=today&v=2&alt=jsonc'},
-  {'title':'Games', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Games?time=today&v=2&alt=jsonc'},
-  {'title':'Movies', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Movies?time=today&v=2&alt=jsonc'},
-  {'title':'Music', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Music?time=today&v=2&alt=jsonc'},
-  {'title':'News', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_News?time=today&v=2&alt=jsonc'},
-  {'title':'Sports', 'url':'https://gdata.youtube.com/feeds/api/standardfeeds/most_popular_Sports?time=today&v=2&alt=jsonc'},
+  {'title':'Popular', 'url':'https://www.googleapis.com/youtube/v3/videos?maxResults=50&key=AIzaSyAzkfoVmKXf3520e5WLBMnOMXXbyjIMJLk&part=snippet&chart=mostPopular'},
 ]
 
 def name():
@@ -38,26 +30,25 @@ def feed(idx):
     return _extract(data)
 
 def search(q):
-  query = {'format':'5', 'v':'2', 'alt':'jsonc', 'q': q}
+  query = {'part':'snippet', 'q':q, 'maxResults': 50,
+	    'key':'AIzaSyAzkfoVmKXf3520e5WLBMnOMXXbyjIMJLk'}
   data = get_json(_SEARCH_URL, params=query)
   return _extract(data)
 
 def _extract(data):
   results = PlayItemList()
-  if not 'data' in data or data['data']['totalItems'] == 0:
-    return results
-  rtree = data['data']['items']
+  rtree = data['items']
   for r in rtree:
-    title = r['title']
-    # Ignore device support video in searches
-    if title == 'https://youtube.com/devicesupport':
-      continue
-    img = r['thumbnail']['sqDefault']
-    url = r['player']['default']
-    m, s = divmod(r['duration'], 60)
-    h, m = divmod(m, 60)
-    subtitle = 'Duration: ' + "%d:%02d:%02d" % (h, m, s)
-    if 'viewCount' in r:
-      subtitle = subtitle + ', Views: ' + "{:,}".format(r['viewCount'])
-    results.add(PlayItem(title, img, url, subtitle))
+    title= r['snippet']['title']
+    subtitle= r['snippet']['publishedAt'][:10]
+    synopsis= r['snippet']['description']
+    if len(synopsis) > 200:
+      synopsis = synopsis[:200] + "..."
+    img = r['snippet']['thumbnails']['default']['url']
+    if isinstance(r['id'], basestring):
+      vid = r['id']
+    else:
+      vid = r['id']['videoId']
+    url = 'https://www.youtube.com/watch?v=' + vid
+    results.add(PlayItem(title, img, url, subtitle, synopsis))
   return results
