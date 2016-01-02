56dde83edd481b9b5c51f05aff6b7e3c6a4b90b9
Blissland <blissland.house@googlemail.com>
2016-01-02 14:16:09 +0000

    Add feeds to ITV player

--- chls/bfch_itv_player/__init__.py
+++ chls/bfch_itv_player/__init__.py
@@ -3,7 +3,15 @@ from chanutils import get_attr, get_text, get_text_content
 from playitem import PlayItem, PlayItemList, MoreEpisodesAction
 
 _FEEDLIST = [
-  {'title':'Shows', 'url':'http://www.itv.com/hub/shows'},
+  {'title':'All Shows', 'url':'http://www.itv.com/hub/shows'},
+  {'title':'Popular', 'url':'http://www.itv.com'},
+  {'title':'Children', 'url':'http://www.itv.com/hub/categories/children'},
+  {'title':'Comedy', 'url':'http://www.itv.com/hub/categories/comedy'},
+  {'title':'Drama & Soaps', 'url':'http://www.itv.com/hub/categories/drama-and-soaps'},
+  {'title':'Entertainment', 'url':'http://www.itv.com/hub/categories/entertainment'},
+  {'title':'Factual', 'url':'http://www.itv.com/hub/categories/factual'},
+  {'title':'Films', 'url':'http://www.itv.com/hub/categories/films'},
+  {'title':'Sport', 'url':'http://www.itv.com/hub/categories/sport'},
 ]
 
 _SHOWLIST = []
@@ -27,19 +35,23 @@ def feed(idx):
   results = PlayItemList()
   for l in rtree:
     url = get_attr(l, 'href')
-    el = select_one(l, 'h3.tout__title')
+    el = select_one(l, '.tout__title')
+    if el is None:
+      continue
     title = get_text(el)
     el = select_one(l, 'img.fluid-media__media')
     img = get_attr(el, 'src')
     el = select_one(l, 'p.tout__meta')
-    subtitle = get_text(el)
+    subtitle = get_text_content(el)
     if subtitle == 'No episodes available':
       continue    
     item = PlayItem(title, img, url, subtitle)
-    item.add_action(MoreEpisodesAction(url, title))
+    if subtitle != '1 episode':
+      item.add_action(MoreEpisodesAction(url, title))
     results.add(item)
-  global _SHOWLIST
-  _SHOWLIST = results
+  if idx == 0:
+    global _SHOWLIST
+    _SHOWLIST = results
   return results
 
 def search(q):
