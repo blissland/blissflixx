9375430bb8071593b5db331152f787483b666c06
blissland <blissland.house@googlemail.com>
2016-03-04 20:58:06 +0000

    Remove wificonf and document boot script

--- boot.sh
+++ boot.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#
+# Run this script if you want to make Blissflixx
+# automatically run on boot. 
+#
+# Edit the following file:
+#   sudo vi /etc/init.d/superscript
+#
+# Add these lines to the file:
+#   #!/bin/bash
+#   cd /home/pi/blissflixx
+#   sudo -u pi "./boot.sh"
+#
+# Now make the script executable:
+#   sudo chmod 755 /etc/init.d/superscript
+# 
+# Thats it. Blissflixx should now run on boot
+#
 echo "------------------------------------------"
 echo "Waiting for 10 seconds for WIFI to connect"
 echo "------------------------------------------"
 sleep 10
 ./blissflixx.py --port 80 --daemon
-./wificonf.py
