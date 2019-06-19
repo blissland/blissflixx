a1043a66b783b793b87f8b33cde70e650d16881c
Diego Rodríguez <diegorodriguezv@gmail.com>
2019-06-18 23:45:20 -0500

    Integrate some community plugins
            chls/bfch_eztv/
            chls/bfch_kickass_torrents/
            chls/bfch_local_media/
            chls/bfch_pirate_bay/
            chls/bfch_yts_torrents/

---
+++ chls/bfch_local_media/config.py
@@ -0,0 +1,10 @@
+# To add your own media files to Blissflixx:
+# Copy this file to a file called config.py
+# Edit the _FEEDLIST below with a list of your local media folders
+# Restart Blissflixx
+# Enjoy !
+
+_FEEDLIST = [
+
+  {'title':'Torrents','folder':'/mnt/My_Book/Usuarios/Diego/Descargas/Torrents'},
+]
