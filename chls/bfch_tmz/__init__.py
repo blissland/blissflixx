e9a15c01da9fcb7ce3b2f7646438352d4e759db4
Diego Rodríguez <diegorodriguezv@gmail.com>
2020-04-03 13:58:07 -0500

    Fix TMZ plugin by screen scraping

--- chls/bfch_tmz/__init__.py
+++ chls/bfch_tmz/__init__.py
@@ -1,28 +1,27 @@
 import chanutils.reddit
-from chanutils import get_json, replace_entity
+from chanutils import get_doc, select_all, select_one, get_attr, get_text
 from playitem import PlayItem, PlayItemList
 from urllib import quote
 
-_SEARCH_URL = "http://www.tmz.com/search/json/videos/"
+_SEARCH_URL = "https://www.tmz.com/search/videos/"
 
 _FEEDLIST = [
-  {'title':'Latest', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1'},
-  {'title':'Beauty', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=beauty'},
-  {'title':'Celeb Justice', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=celebrity-justice'},
-  {'title':'Dating', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=dating'},
-  {'title':'Family', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=family'},
-  {'title':'Fashion', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=fashion'},
-  {'title':'Fights', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=fights'},
-  {'title':'Hook Ups', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=hook-ups'},
-  {'title':'LGBT', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=lgbt'},
-  {'title':'Money', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=money'},
-  {'title':'Movies', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=movies'},
-  {'title':'Music', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=music'},
-  {'title':'Relationships', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=relationships'},
-  {'title':'Sex', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=sex'},
-  {'title':'Sports', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=tmzsports'},
-  {'title':'Television', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=tv'},
-  {'title':'Vacations', 'url':'http://www.tmz.com/video-feed-categories/tmz-owned.json?pagesize=30&page=1&tmz-category=vacations'},
+  {'title':'Beauty', 'url':'http://www.tmz.com/categories/beauty/videos/'},
+  {'title':'Celeb Justice', 'url':'http://www.tmz.com/categories/celebrity-justice/videos/'},
+  {'title':'Dating', 'url':'http://www.tmz.com/categories/dating/videos/'},
+  {'title':'Family', 'url':'http://www.tmz.com/categories/family/videos/'},
+  {'title':'Fashion', 'url':'http://www.tmz.com/categories/fashion/videos/'},
+  {'title':'Fights', 'url':'http://www.tmz.com/categories/fights/videos/'},
+  {'title':'Hook Ups', 'url':'http://www.tmz.com/categories/hook-ups/videos/'},
+  {'title':'LGBT', 'url':'http://www.tmz.com/categories/lgbt/videos/'},
+  {'title':'Money', 'url':'http://www.tmz.com/categories/money/videos/'},
+  {'title':'Movies', 'url':'http://www.tmz.com/categories/movies/videos/'},
+  {'title':'Music', 'url':'http://www.tmz.com/categories/music/videos/'},
+  {'title':'Relationships', 'url':'http://www.tmz.com/categories/relationships/videos/'},
+  {'title':'Sex', 'url':'http://www.tmz.com/categories/sex/videos/'},
+  {'title':'Sports', 'url':'http://www.tmz.com/categories/tmzsports/videos/'},
+  {'title':'Television', 'url':'http://www.tmz.com/categories/tv/videos/'},
+  {'title':'Vacations', 'url':'http://www.tmz.com/categories/vacations/videos/'},
 ]
 
 def name():
@@ -38,22 +37,24 @@ def feedlist():
   return _FEEDLIST
 
 def feed(idx):
-  data = get_json(_FEEDLIST[idx]['url'])
-  return _extract(data)
+  doc = get_doc(_FEEDLIST[idx]['url'])
+  return _extract(doc)
 
 def search(q):
-  data = get_json(_SEARCH_URL + quote(q) + '/1.json')
-  return _extract(data)
+  doc = get_doc(_SEARCH_URL + '?q=' + quote(q))
+  return _extract(doc)
 
-def _extract(data):
+def _extract(doc):
   results = PlayItemList()
-  rtree = data['results']
+  rtree = select_all(doc, 'div.col')
   for r in rtree:
-    title = replace_entity(r['title'])
-    img = r['thumbnailUrl']
-    if 'url' in r:
-      url = r['videoUrl']
-    else:
-      url = r['URL']
+    el = select_one(r, "h4")
+    title = get_text(el)
+    if not title:
+      continue
+    el = select_one(r, "img")
+    img = get_attr(el, "src")
+    el = select_one(r, "a.js-click-video")
+    url = get_attr(el, "href")
     results.add(PlayItem(title, img, url))
   return results
