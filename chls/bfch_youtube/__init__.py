3e0e03cd72ab4f323cea060107104c105711c718
Diego Rodríguez <diegorodriguezv@gmail.com>
2020-05-08 17:15:39 -0500

    Change set of YT API keys

--- chls/bfch_youtube/__init__.py
+++ chls/bfch_youtube/__init__.py
@@ -6,11 +6,24 @@ from playitem import PlayItem, PlayItemList
 _SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
 _API_KEY_POOL = [
-  "AIzaSyC76A-MHHypf-kv-4l-dEPPCK65C38lMt4", # newbliss
-  "AIzaSyArJTw6o9Tl6DLrETTxHXmxOEmuPcJUuQk", # blissflixx1
-  "AIzaSyBFQq1lIlQKlGRvCu45RhlDTXx4bGziByY", # blissflixx2
-  "AIzaSyCOwS8E3Vr70bj5dQqXJX6PPnla9j7VokA", # blissflixx3
-  "AIzaSyAw10CnwxuKV2Z9vW6ehXUDPslpCgQ93NA", # blissflixx4
+#  "AIzaSyC76A-MHHypf-kv-4l-dEPPCK65C38lMt4", # newbliss
+#  "AIzaSyArJTw6o9Tl6DLrETTxHXmxOEmuPcJUuQk", # blissflixx1
+#  "AIzaSyBFQq1lIlQKlGRvCu45RhlDTXx4bGziByY", # blissflixx2
+#  "AIzaSyCOwS8E3Vr70bj5dQqXJX6PPnla9j7VokA", # blissflixx3
+#  "AIzaSyAw10CnwxuKV2Z9vW6ehXUDPslpCgQ93NA", # blissflixx4
+  "AIzaSyB9i9vu-Kwd7kiKwDalH06fMjLjYeylZsg", # test0
+  "AIzaSyBkr7KZdZ33IKcdXWb8RmQZZQB3K8Uow0s", # test1
+  "AIzaSyAYg7qdi6SnSjJUgiccOXSyOoxlAG0wHIw", # test2
+  "AIzaSyBt-OPrCrXF0oIf9FkXRdd142f7cDdZubk", # test3
+  "AIzaSyAdRojg4iwdHY6wvirXhikfFXTZ0rpgfNg", # test4
+  "AIzaSyCof4H5PpGCq5NSdqNEWgM7p1rNgzTTR-0", # test5
+  "AIzaSyBxZbYSU7V4cG73Gnj3MjlbSPAWsV1Rugs", # test6
+  "AIzaSyCh0nZGziipkXz8FdVlLQ2ea4kXBtD4ku0", # test7
+  "AIzaSyAdF6_MjwSo0r3q40xj7NlySvaM0kCdfb0", # test8
+  "AIzaSyBO0U1YrpT6Gf3Ehz3UfuKjzi8sQdKUcgc", # test9
+  "AIzaSyB7Jn8hKG-vc4snUrZVcZ25R8UnhmeRLBM", # test10
+  "AIzaSyC55Q8NmG72-KqiEru54CpcRIrwcu5yRFU", # test11
+
 ]
 
 _API_KEY = random.choice(_API_KEY_POOL)
