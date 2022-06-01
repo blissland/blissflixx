a88a770ab732107a2aa05ef741acc1a52af3685d
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2022-06-01 15:39:48 -0500

    Improve documentation for configure.sh

--- configure.sh
+++ configure.sh
@@ -15,38 +15,40 @@ echo "Installing system dependencies... (This will take a while)"
 echo ""
 echo "============================================================"
 
-# Update to latest packages
+# Update software repositories 
 apt update
 
-# Install npm 
+# This includes nodejs
 apt -y install npm
 
-# Install latest omxplayer
-apt-get -y install omxplayer
+# Install raspberry pi optimized video player
+apt -y install omxplayer
 
-# Install GIT
+# Install version control software for updates and to install other packages like
+# youtube-dl. It is probably already installed, but it is possible that the user
+# got blissflixx other way, like downloading the source as a zip file.
 apt -y install git
 
-# Install python pip
+# Install python package manager
 apt -y install python3-pip
 
-# Install python venv
+# Install python virtual environment to keep blissflixx dependencies isolated
 apt -y install python3-venv
 
-# Install XML parser
+# Install XML parser for screen scaping websites
 apt -y install libxml2-dev
 
-# Install XSLT package
+# Install XSLT package for screen scaping websites
 apt -y install libxslt1-dev
 
 # So server can run on port 80 without sudo
 setcap 'cap_net_bind_service=+ep' "$(readlink -f "$(which python3)")"
 
-user=$(logname)
-
-# Install peerflix
+# Install peerflix globally
 npm install -g peerflix
 
-# Configure python dependencies
-sudo -u $user ./configure_py.sh
+# The user that started the command (since sudo was used, root is the active user)
+user=$(logname)
 
+# Configure python dependencies as a normal user
+sudo -u $user ./configure_py.sh
