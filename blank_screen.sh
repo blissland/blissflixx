fffbe85009644f4248d1ef4af2de7648d3b9556a
diegorodriguezv <diegorodriguezv@gmail.com>
2022-06-04 03:10:49 -0500

    Fix blank screen without x

--- blank_screen.sh
+++ blank_screen.sh
@@ -4,7 +4,7 @@
 tvservice -p
 
 # Set the screensaver timeout when an x environment exists
-until which xinit && DISPLAY=:0 xset dpms 10 10 10
+until ! which xinit || DISPLAY=:0 xset dpms 10 10 10
 do
   echo '.'
   sleep 1
